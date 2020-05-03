059beb209c492cc6e26da66673827f42989a6599
Petru Flueras <petru.flueras@gmail.com>
2020-05-03 20:41:06 +0300

    Applied auto formatting

--- rpi/car/camera.py
+++ rpi/car/camera.py
@@ -1,10 +1,8 @@
-
 class Camera:
     def __init__(self, resolution_x, resolution_y, rotation):
         self._resolution_x = resolution_x
         self._resolution_y = resolution_y
         self._rotation = rotation
-
 
     def take_picture(self, image_filename):
         print('Taking a picture. Saving it to ' + image_filename)
--- rpi/car/motor.py
+++ rpi/car/motor.py
@@ -1,18 +1,14 @@
-
 class Motor:
 
     def __init__(self, forward_pin, backward_pin):
         self.forward_pin = forward_pin
         self.backward_pin = backward_pin
 
-
     def move_forward(self):
         pass
 
-
     def move_backward(self):
         pass
-
 
     def stop(self):
         pass
