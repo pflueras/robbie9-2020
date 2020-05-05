88aa658489444141eb8e6c1e67caf53e0fa20744
Alexandra <alexandra.tripcea@gmail.com>
2020-05-05 19:34:39 +0300

    Added documentation for Motor. Closes #3

--- rpi/car/motor.py
+++ rpi/car/motor.py
@@ -1,14 +1,21 @@
 class Motor:
+    """ Class destined for handling the motors behaviour. """
 
     def __init__(self, forward_pin, backward_pin):
         self.forward_pin = forward_pin
         self.backward_pin = backward_pin
 
     def move_forward(self):
+        """ The motor starts. """
+
         pass
 
     def move_backward(self):
+        """ The motor starts. """
+
         pass
 
     def stop(self):
+        """ The motor stops. """
+
         pass
