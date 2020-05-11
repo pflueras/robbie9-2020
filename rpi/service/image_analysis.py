0f00bc957830c6d21e7d1142a3d3342b6497351d
Alex Prapugicu <40821161+AlexPrapugicu@users.noreply.github.com>
2020-05-11 09:57:58 +0300

    Update rpi/service/image_analysis.py
    Co-authored-by: pflueras <petru.flueras@gmail.com>

--- rpi/service/image_analysis.py
+++ rpi/service/image_analysis.py
@@ -5,7 +5,7 @@ class ImageAnalysisService:
 def upload_image(self, image_filename: str)
     def uploadImage(self, image_filename: str):
         time.sleep(0.25)
-
+def detect_traffic_light(self, image_filename) -> bool:
     def detectTrafficLight(self, image_filename) -> bool:
         time.sleep(0.25)
         return True
