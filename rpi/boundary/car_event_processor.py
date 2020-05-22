4121b3061618d891ea58944fb9c1c485561b14b8
Alexandra <alexandra.tripcea@gmail.com>
2020-05-22 21:33:13 +0300

    Publish events on each car change of status. Closes #28

--- rpi/boundary/car_event_processor.py
+++ rpi/boundary/car_event_processor.py
@@ -1,59 +1,51 @@
 from pydispatch import dispatcher
-from shared import status_signals
+
+from car.car_status import CarStatus
+
 
 class CarEventProcessor:
     def __init__(self):
-        dispatcher.connect(self.running_dispatcher_recieve,
-                           signal=status_signals.RUNNING_SIGNAL,
-                           sender=status_signals.RUNNING_SENDER)
-        dispatcher.connect(self.stopped_dispatcher_recieve,
-                           signal=status_signals.STOPPED_SIGNAL,
-                           sender=status_signals.STOPPED_SENDER)
-        dispatcher.connect(self.take_picture_dispatcher_recieve,
-                           signal=status_signals.TAKE_PICTURE_SIGNAL,
-                           sender=status_signals.TAKE_PICTURE_SENDER)
-        dispatcher.connect(self.uploading_image_dispatcher_recieve,
-                           signal=status_signals.UPLOAD_IMAGE_SIGNAL,
-                           sender=status_signals.UPLOAD_IMAGE_SENDER)
-        dispatcher.connect(self.image_uploaded_dispatcher_recieve,
-                           signal=status_signals.IMAGE_UPLOADED_SIGNAL,
-                           sender=status_signals.IMAGE_UPLOADED_SENDER)
-        dispatcher.connect(self.analyze_image_dispatcher_recieve,
-                           signal=status_signals.ANALYZE_IMAGE_SIGNAL,
-                           sender=status_signals.ANALYZE_IMAGE_SENDER)
-        dispatcher.connect(self.traffic_light_detected_dispatcher_recieve,
-                           signal=status_signals.TRAFFIC_LIGHT_DETECTED_SIGNAL,
-                           sender=status_signals.TRAFFIC_LIGHT_DETECTED_SENDER)
-        dispatcher.connect(self.traffic_light_not_present_dispatcher_recieve,
-                           signal=status_signals.TRAFFIC_LIGHT_NOT_PRESENT_SIGNAL,
-                           sender=status_signals.TRAFFIC_LIGHT_NOT_PRESENT_SENDER)
-        dispatcher.connect(self.move_forward_dispatcher_recieve,
-                           signal=status_signals.MOVE_FORWARD_SIGNAL,
-                           sender=status_signals.MOVE_FORWARD_SENDER)
+        dispatcher.connect(self.running, signal=CarStatus.RUNNING, sender=dispatcher.Any)
 
-    def take_picture_dispatcher_recieve(self):
+        dispatcher.connect(self.stopped, signal=CarStatus.STOPPED, sender=dispatcher.Any)
+
+        dispatcher.connect(self.take_picture, signal=CarStatus.TAKE_PICTURE, sender=dispatcher.Any)
+
+        dispatcher.connect(self.uploading_image, signal=CarStatus.UPLOADING_IMAGE, sender=dispatcher.Any)
+
+        dispatcher.connect(self.image_uploaded, signal=CarStatus.IMAGE_UPLOADED, sender=dispatcher.Any)
+
+        dispatcher.connect(self.analyse_image, signal=CarStatus.ANALYSE_IMAGE, sender=dispatcher.Any)
+
+        dispatcher.connect(self.traffic_light_detected, signal=CarStatus.TRAFFIC_LIGHT_DETECTED, sender=dispatcher.Any)
+
+        dispatcher.connect(self.traffic_light_not_present, signal=CarStatus.TRAFFIC_LIGHT_NOT_PRESENT, sender=dispatcher.Any)
+
+        dispatcher.connect(self.move_forward, signal=CarStatus.MOVE_FORWARD, sender=dispatcher.Any)
+
+    def take_picture(self):
         print('took picture')
 
-    def uploading_image_dispatcher_recieve(self):
+    def uploading_image(self):
         print('uploaded image')
 
-    def traffic_light_detected_dispatcher_recieve(self):
+    def traffic_light_detected(self):
         print('detected traffic light')
 
-    def traffic_light_not_present_dispatcher_recieve(self):
+    def traffic_light_not_present(self):
         print('traffic light not detected')
 
-    def move_forward_dispatcher_recieve(self):
+    def move_forward(self):
         print('moving forward')
 
-    def running_dispatcher_recieve(self):
+    def running(self):
         print('running')
 
-    def stopped_dispatcher_recieve(self):
+    def stopped(self):
         print('stopped')
 
-    def image_uploaded_dispatcher_recieve(self):
+    def image_uploaded(self):
         print('image uploaded')
 
-    def analyze_image_dispatcher_recieve(self):
+    def analyse_image(self):
         print('image analize')
--- rpi/service/car_thread.py
+++ rpi/service/car_thread.py
@@ -2,8 +2,8 @@ import time
 from threading import Thread
 from car.car import Car
 from pydispatch import dispatcher
+from car.car_status import CarStatus
 from service.image_analysis import ImageAnalysisService
-from shared import status_signals
 
 class CarThread(Thread):
     def __init__(self, car: Car, image_analysis_service: ImageAnalysisService):
@@ -17,31 +17,23 @@ class CarThread(Thread):
             image_taken = 'img.png'
 
             self._car.take_picture(image_taken)
-            dispatcher.send(message='taking image',
-                            signal=status_signals.TAKE_PICTURE_SIGNAL,
-                            sender=status_signals.TAKE_PICTURE_SENDER)
+            dispatcher.send(signal=CarStatus.TAKE_PICTURE, sender=dispatcher.Any)
 
             self._imageAnalysisService.upload_image(image_taken)
-            dispatcher.send(message='uploading image',
-                            signal=status_signals.UPLOAD_IMAGE_SIGNAL,
-                            sender=status_signals.UPLOAD_IMAGE_SENDER)
+            dispatcher.send(signal=CarStatus.UPLOADING_IMAGE, sender=dispatcher.Any)
 
             if (self._imageAnalysisService.detect_traffic_light(image_taken)):
-                dispatcher.send(message='traffic light detected',
-                                signal=status_signals.TRAFFIC_LIGHT_DETECTED_SIGNAL,
-                                sender=status_signals.TRAFFIC_LIGHT_DETECTED_SENDER)
+                dispatcher.send(signal=CarStatus.TRAFFIC_LIGHT_DETECTED, sender=dispatcher.Any)
             else:
-                dispatcher.send(message='traffic light not detected',
-                                signal=status_signals.TRAFFIC_LIGHT_NOT_PRESENT_SIGNAL,
-                                sender=status_signals.TRAFFIC_LIGHT_NOT_PRESENT_SENDER)
+                dispatcher.send(signal=CarStatus.TRAFFIC_LIGHT_NOT_PRESENT, sender=dispatcher.Any)
 
             self._car.move_forward()
-            dispatcher.send(message='moving forward',
-                            signal=status_signals.MOVE_FORWARD_SIGNAL,
-                            sender=status_signals.MOVE_FORWARD_SENDER)
+            dispatcher.send(signal=CarStatus.MOVE_FORWARD, sender=dispatcher.Any)
 
             time.sleep(1)
+
         self._car.stop()
+        dispatcher.send(signal=CarStatus.STOPPED, sender=dispatcher.Any)
 
     def stop(self):
         self._running = False
