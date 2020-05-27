6403d2a4143b9cbbc7e873a13b23059f8d63c94d
NPseba <neteda.sebastian@gmail.com>
2020-05-27 10:25:45 +0300

    Replace send with emit. Closes #30

--- rpi/boundary/car_event_processor.py
+++ rpi/boundary/car_event_processor.py
@@ -19,44 +19,45 @@ class CarEventProcessor:
 
         dispatcher.connect(self.traffic_light_detected, signal=CarStatus.TRAFFIC_LIGHT_DETECTED, sender=dispatcher.Any)
 
-        dispatcher.connect(self.traffic_light_not_present, signal=CarStatus.TRAFFIC_LIGHT_NOT_PRESENT, sender=dispatcher.Any)
+        dispatcher.connect(self.traffic_light_not_present, signal=CarStatus.TRAFFIC_LIGHT_NOT_PRESENT,
+                           sender=dispatcher.Any)
 
         dispatcher.connect(self.move_forward, signal=CarStatus.MOVE_FORWARD, sender=dispatcher.Any)
 
         self._socketio = socketio
 
     def take_picture(self):
-        self._socketio.send('took picture')
+        self._socketio.emit('take_picture', 'took picture', broadcast=True)
         print('took picture')
 
     def uploading_image(self):
-        self._socketio.send('uploaded image')
+        self._socketio.emit('uploading_image', 'uploaded image', broadcast=True)
         print('uploaded image')
 
     def traffic_light_detected(self):
-        self._socketio.send('detected traffic light')
+        self._socketio.emit('traffic_light_detected', 'detected traffic light', broadcast=True)
         print('detected traffic light')
 
     def traffic_light_not_present(self):
-        self._socketio.send('traffic light not detected')
+        self._socketio.emit('traffic_light_not_present', 'traffic light not detected', broadcast=True)
         print('traffic light not detected')
 
     def move_forward(self):
-        self._socketio.send('moving forward')
+        self._socketio.emit('move_forward', 'moving forward', broadcast=True)
         print('moving forward')
 
     def running(self):
-        self._socketio.send('running')
+        self._socketio.emit('running', 'running', broadcast=True)
         print('running')
 
     def stopped(self):
-        self._socketio.send('stopped')
+        self._socketio.emit('stopped', 'stopped', broadcast=True)
         print('stopped')
 
     def image_uploaded(self):
-        self._socketio.send('image uploaded')
+        self._socketio.emit('image_uploaded', 'image uploaded', broadcast=True)
         print('image uploaded')
 
     def analyse_image(self):
-        self._socketio.send('image analise')
+        self._socketio.emit('analyse_image', 'image analise', broadcast=True)
         print('image analise')
